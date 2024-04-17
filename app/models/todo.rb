84d6bc70e796967f61e734823bf4cfee6eb18594
shendrikennethyamba <165635645+shendrikennethyamba@users.noreply.github.com>
2024-04-17 17:33:28 +0800

    Update todo.rb

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,5 @@
 class Todo < ApplicationRecord
+  include ActiveModel::Validations
+  validates :title, presence: true, length: { minimum: 10 }
+  validates :body, presence: true, length: { minimum: 10 }
 end
